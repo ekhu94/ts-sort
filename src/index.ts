46d806ff5d7cf0e04c70c04973a5db253616cf3a
Erik Huang <ekhu94@gmail.com>
2021-05-10 08:42:37 -0700

    adds type guards to sort function in bad implementation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { collection } = this;
     for (let i = collection.length - 1; i > 0; i--) {
       for (let j = 0; j < i; j++) {
-        if (collection[j] > collection[j + 1]) {
-          const temp = collection[j];
-          collection[j] = collection[j + 1];
-          collection[j + 1] = temp;
+        if (collection instanceof Array) {
+          if (collection[j] > collection[j + 1]) {
+            const temp = collection[j];
+            collection[j] = collection[j + 1];
+            collection[j + 1] = temp;
+          }
+        }
+
+        if (typeof collection === 'string') {
         }
       }
     }
